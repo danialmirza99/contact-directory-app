03812d0c1cb6a244e4d604a92390205f575c7fe1
danialmirza99 <danialmirza99@gmail.com>
2022-11-29 12:01:31 -0800

    recorded data for entered name

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,49 +1,50 @@
-// TODO: Install the following package:
+
 import { openDB } from 'idb';
 
-// TODO: Complete the initDb() function below:
+
 const initdb = async () =>
-  openDB('todosDB', 1, {
+  openDB('contactsDB', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('todosDB')) {
-        console.log('todos database already exists');
+      if (db.objectStoreNames.contains('contactsDB')) {
+        console.log('contacts database already exists');
         return;
       }
-      db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true });
-      console.log('todos database created');
+      db.createObjectStore('contacts', 'homePhones', 'cellPhones', 'emails', { keyPath: 'id', autoIncrement: true });
+      console.log('contacts database created');
     },
   });
 
 
-// TODO: Complete the postDb() function below:
+
 export const postDb = async (content) => {
     console.log('Post to the ase');
-    const todosDb = await openDB('todosDB', 1);
-    const tx = todosDb.transaction('todos', 'readwrite');
-    const store = tx.objectStore('todos');
-    const request = store.add({ todo: content });
+    const contactsDB = await openDB('contactsDB', 1);
+    const tx = contactsDB.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts', 'homePhones', 'cellPhones', 'emails');
+    const request = store.add({ name: content, homePhone: content, cellPhone: content, email: content });
+    console.log("This is my log:" + content);
     const result = await request;
     console.log('🚀 - data saved to the database', result);
   };
 
-// TODO: Complete the getDb() function below:
+
 export const getDb = async () => {
     console.log('GET from the database');
-    const todosDb = await openDB('todosDB', 1);
-    const tx = todosDb.transaction('todos', 'readonly');
-    const store = tx.objectStore('todos');
+    const contactsDB = await openDB('contactsDB', 1);
+    const tx = contactsDB.transaction('contacts', 'readonly');
+    const store = tx.objectStore('contacts', 'homePhones', 'cellPhones', 'emails');
     const request = store.getAll();
     const result = await request;
     console.log('result.value', result);
     return result;
 };
 
-// TODO: Complete the deleteDb() function below:
+
 export const deleteDb = async (id) => {
     console.log('DELETE from the database', id);
-    const todosDb = await openDB('todosDB', 1);
-    const tx = todosDb.transaction('todos', 'readwrite');
-    const store = tx.objectStore('todos');
+    const contactsDB = await openDB('contactsDB', 1);
+    const tx = contactsDB.transaction('contacts', 'readwrite');
+    const store = tx.objectStore('contacts', 'homePhones', 'cellPhones', 'emails');
     const request = store.delete(id);
     const result = await request;
     console.log('result.value', result);
